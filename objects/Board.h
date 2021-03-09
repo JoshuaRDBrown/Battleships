fe56487d898bcb5e4fe2e33dec5fcba54a4848d7
Joshua Brown <joshua.rd.brown2001@gmail.com>
2021-03-09 21:52:40 +0000

    Fix indentation

--- objects/Board.h
+++ objects/Board.h
@@ -19,24 +19,32 @@ class Board {
 		char columnLetters[26] = {
 			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 		};
+		
+		enum randomGenerationType {
+			COORD,
+			ORIENTATION
+		};
 
 		int height;
 		int width;
 		std::map<std::string, int> shipData;
 		std::vector<Ship*> placedShips = { }; 
+		bool isComputerBoard;
 
 	public: 
 
-		Board(int boardHeight, int  boardWidth, std::map<std::string, int> ships) {
+		Board(int boardHeight, int  boardWidth, std::map<std::string, int> ships, bool isComputer) {
 			height = boardHeight;
 			width = boardWidth;
 			shipData = ships;
+			isComputerBoard = isComputer;
     }
 
 		void drawBoard(bool isInitalDraw);
 		BoardDimensions getBoardDimensions();
 		std::map<std::string, int> getShipData();
 		bool coordinateIsValid(std::string coordinates);
-		void handleBoatPlacementInput();
+		int handleBoatPlacementInput();
 		shipPlacementStatus attemptShipPlacement(std::string shipName, int shipLength, std::string coordinates, std::string orientation);
+		std::string generateRandomPlacement(randomGenerationType genType);
 };
