8a8d6ac6cbb5868aa7887bea9cedf0fcbd63ee99
Joshua Brown <joshua.rd.brown2001@gmail.com>
2021-03-09 21:53:25 +0000

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -8,4 +8,5 @@ class Utils {
 		std::vector<std::string> parseGameConfigFile(const std::string& data, const std::string& seperator);
 		std::string convertStringToUpperCase(std::string str);
 		RowAndCol getIndexFromCoordinates(char *columnLetters, std::string coordinates);
+		int randomNumber(int range);
 };
